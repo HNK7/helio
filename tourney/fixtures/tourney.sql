db8e6427807dc95d1dd0a3aeacda116b9479b26d
Stu Pae <stu@dartoo.com>
2013-03-28 22:18:49 -0400

    .

--- tourney/fixtures/tourney.sql
+++ tourney/fixtures/tourney.sql
@@ -1,3 +1,3 @@
 insert into tourney_tournament (title, start_at, end_at) values ('Lansdale - PA', '2013-01-25'::timestamp, '2013-01-27'::timestamp);
 
-insert into tourney_tournament (title, start_at, end_at) values ('Pittsburgh - PA', '2013-01-25'::timestamp, '2013-01-27'::timestamp);
+insert into tourney_tournament (title, start_at, end_at) values ('Pittsburgh - PA', '2013-03-29'::timestamp, '2013-03-31'::timestamp);
