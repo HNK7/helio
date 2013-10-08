51afef7338b072fd8914924b87ce4ea5ee901f2e
Stu Pae <sopae@Stus-MacBook-Pro.local>
2013-10-08 04:52:25 -0700

    .

--- tourney/views.py
+++ tourney/views.py
@@ -708,7 +708,7 @@ def event_signup(request, e_id):
                                 messages.error(request, '%s has already signed up!' % (player.full_name))
                                 return HttpResponseRedirect(reverse('22k:event_signup', args=[e_id]))
                         if event.is_official() and  not player.is_qualified(event.tournament):
-                                messages.error(request, '%s is qualified!' % (player.full_name))
+                                messages.error(request, '%s is NOT qualified for this event!' % (player.full_name))
                                 return HttpResponseRedirect(reverse('22k:event_signup', args=[e_id]))
 
                         players.append(player)
