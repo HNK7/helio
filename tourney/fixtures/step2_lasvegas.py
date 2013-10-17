324c0257cf039fb7682d2823544cce5a3aefaf1d
Stu Pae <stu.pae@gmail.com>
2013-10-16 17:40:57 -0700

    .

--- tourney/fixtures/step2_lasvegas.py
+++ tourney/fixtures/step2_lasvegas.py
@@ -12,7 +12,7 @@ from django.core.management import setup_environ
 setup_environ(local)
 # setup_environ(production)
 from tourney.models import PreRegVegas, Player, Card, Entry, Tournament
-from tourney.phoenixcard import PhoenixCard
+from tourney.phoenixcard import PhoenixCard, PhoenixLeagueCard
 from decimal import Decimal
 
 counter = {'22k': 0, 'casual_invalid': 0, 'league_invalid': 0}
@@ -22,30 +22,82 @@ vegas_tourney = Tournament.objects.get(id=5)
 for no, reg in enumerate(PreRegVegas.objects.all(), start=1):
 
     line = str(no) + ': ' + ' '.join([reg.first_name, reg.last_name])
-    if reg.casual_card:
-        pxcard = PhoenixCard(cardno=reg.casual_card)
-        # pxcard = PhoenixCard(cardno=1223)
+    if reg.casual_card and reg.league_card:
+
         try:
-            rfid = pxcard.get_rfid()
-            # org_rfid = pxcard.get_org_rfid()
+            pxcard = PhoenixCard(cardno=reg.casual_card)
+            plcard = PhoenixLeagueCard(cardno=reg.league_card)
         except Exception, e:
+            pass
             print line, e
-            counter['casual_invalid'] += 1
             continue
+        ppd_diff = pxcard.ppd - plcard.ppd
+        mpr_diff = pxcard.mpr - plcard.mpr
+        # print line, pxcard.ppd, pxcard.mpr, ppd_diff, mpr_diff
+
+        # Create/Update card and player
         try:
             card = Card.objects.get(cardno=pxcard.cardno)
-            # Card.objects.get(rfid=org_rfid)
-            print line, 'already registered'
-            counter['22k'] += 1
-            entry, created = Entry.objects.get_or_create(tournament=vegas_tourney, player=card.player)
-            if reg.balance > 0:
-                entry.balance_signup = Decimal(entry.balance_signup) + Decimal(reg.balance)
+            player = card.player
+            player.first_name = reg.first_name
+            player.last_name = reg.last_name
+            player.save()
+            print 'Updated already registered player'
+        except Exception, e:
+            # player = Player.objects.create(first_name=reg.first_name, last_name=reg.last_name)
+            # player.gender = reg.gender
+            # player.email = reg.email if reg.email else ''
+            # player.phone = reg.phone if reg.phone else ''
+            # player.save()
+            # card = Card.objects.create(cardno=pxcard.cardno, rfid=pxcard.rfid, player=player)
+            # print 'Registered as a new player'
+            # print 'player id: %s' % player.id
+            # print 'card id: %s' % card.id
+            pass
+            print 'New player'
+            continue
+        # Register for 100K
+        entry, created = Entry.objects.get_or_create(tournament=vegas_tourney, player=player)
+        if reg.balance > 0:
+            entry.balance_signup = Decimal(entry.balance_signup) + Decimal(reg.balance)
 
-            entry.qualified = True
-            entry.save()
-            print  entry
-        except Card.DoesNotExist:
-            print line, 'new player'
+        entry.mpr_rank = pxcard.mpr
+        entry.ppd_rank = pxcard.ppd
+        entry.mpr_event = plcard.mpr
+        entry.ppd_event = plcard.ppd
+        entry.qualified = True
+        entry.save()
+        print line, entry
+
+    else:
+        print line, 'skppied: no casual card or league card'
+    # (card_type, rfid, cardno, old_cardno, old_rfid, userinfo_rfid, members_rfid, name,
+    # m_num, utime, f_name, l_name, m_sex, m_email, m_phone, m_id, m_zip, ppd, mpr) = pxcard.get_card_info()
+
+    # if reg.casual_card:
+    #     pxcard = PhoenixCard(cardno=reg.casual_card)
+    #     # pxcard = PhoenixCard(cardno=1223)
+    #     try:
+    #         rfid = pxcard.get_rfid()
+    #         # org_rfid = pxcard.get_org_rfid()
+    #     except Exception, e:
+    #         print line, e
+    #         counter['casual_invalid'] += 1
+    #         continue
+    #     try:
+    #         card = Card.objects.get(cardno=pxcard.cardno)
+    #         # Card.objects.get(rfid=org_rfid)
+    #         print line, 'already registered'
+    #         counter['22k'] += 1
+    #         entry, created = Entry.objects.get_or_create(tournament=vegas_tourney, player=card.player)
+    #         if reg.balance > 0:
+    #             entry.balance_signup = Decimal(entry.balance_signup) + Decimal(reg.balance)
+
+    #         entry.qualified = True
+    #         entry.save()
+    #         print  entry
+    #     except Card.DoesNotExist:
+    #         print line, 'new player'
 
 
 print 'Total: %s' % no
--- tourney/phoenixcard.py
+++ tourney/phoenixcard.py
@@ -1,132 +1,168 @@
-from django.db import connections, transaction
+from django.db import connections
+from decimal import Decimal
+
+cursor = connections['hi'].cursor()
 
 class PhoenixCard:
     def __init__(self, cardno=None, rfid=None):
-        if not cardno and not rfid:
-            raise Exception('cardno or rfid needs to be set')
-        else:
-            if cardno and (len(str(cardno)) != 16 or not str(cardno).isdigit()):
-                raise Exception('invalid card number')
-            if rfid and (len(str(rfid)) != 20 or not str(rfid).isdigit()):
-                raise Exception('invalid rfid')
-            self.cardno = cardno
-            self.rfid = rfid
-        self.cursor = connections['hi'].cursor()
+
+        self._validate_number(cardno, rfid)
+        # verify cardno and rfid combination
+        r = self._verify(cardno, rfid)
+        self.cardno = str(r['cardno'])
+        self.rfid = str(r['rfid'])
+        self.org_rfid = str(r['org_rfid'])
+
+        # set card info
+        (card_type, rfid, cardno, old_cardno, old_rfid, userinfo_rfid, members_rfid, name,
+                m_num, utime, f_name, l_name, m_sex, m_email, m_phone, m_id, m_zip, ppd, mpr) = self._card_info()
+
+        self.nick_name = name
+        self.user_id = m_id
+
+        self.first_name = f_name
+        self.last_name = l_name
+        self.gender = m_sex
+        self.email = m_email
+        self.phone = m_phone
+        self.zip = m_zip
+
+        self.ppd = ppd
+        self.mpr = mpr
+
+        self.card_type = card_type
+        self.is_copied = False if self.rfid == self.org_rfid else True
+        self.is_new = True if self.card_type == 'New' else False
 
     def __str__(self):
         return self.cardno if self.cardno else self.rfid
 
-    def get_cardno(self):
-        if self.cardno:
-            return self.cardno
-        elif self.rfid:
-            try:
-                self.cursor.execute('SELECT cardno from checkrfid where rfid=%s', [self.rfid])
-                r = self.cursor.fetchone()
-            except Exception:
-                raise Exception('invalid rfid number')
-            if r:
-                return str(r[0])
-            else:
-                raise Exception('invalid rfid number')
-        else:
+    def _validate_number(self, cardno, rfid):
+        # check card number format
+        if not cardno and not rfid:
             raise Exception('cardno or rfid needs to be set')
-
-    def get_rfid(self):
-        if self.rfid:
-            return self.rfid
-        elif self.cardno:
-            try:
-                self.cursor.execute('SELECT rfid from checkrfid where cardno=%s', [self.cardno])
-                r = self.cursor.fetchone()
-            except Exception:
-                raise Exception('invalid card number')
-            if r:
-                return str(r[0])
-            else:
-                raise Exception('invalid card number')
         else:
-            raise Exception('cardno or rfid needs to be set')
+            if cardno and (len(str(cardno)) != 16 or not str(cardno).isdigit()):
+                raise Exception('card number format is invalid: %s' % [cardno])
+            if rfid and (len(str(rfid)) != 20 or not str(rfid).isdigit()):
+                raise Exception('rfid format is invalid: %s' % [rfid])
 
-    def get_org_rfid(self):
-        if self.rfid:
-            return self.rfid
-        elif self.cardno:
-            try:
-                self.cursor.execute('SELECT getorigrfid2(rfid) from checkrfid where cardno=%s', [self.cardno])
-                r = self.cursor.fetchone()
-            except Exception:
-                raise Exception('invalid card number')
-            if r:
-                return str(r[0])
-            else:
-                raise Exception('invalid card number')
-        else:
-            raise Exception('cardno or rfid needs to be set')
-
-    def get_stat(self):
+    def _verify(self, cardno, rfid):
         try:
-            self.cursor.execute('SELECT ppd_ta2, mpr_ta2 from useravg a, checkrfid b where a.rfid=getorigrfid2(b.rfid) and (b.cardno=%s or b.rfid=getorigrfid2(%s))', [self.cardno, self.rfid])
-            r = self.cursor.fetchone()
-        except Exception, e:
-            raise Exception('invalid card:%s' % [e])
-        if r:
-            return {'PPD': r[0], 'MPR': r[1]}
-        else:
-            raise Exception('invalid card')
-
-    def is_new(self):
-        try:
-            self.cursor.execute('SELECT rfid from userinfo  where cardno=%s or rfid=getorigrfid2(%s)', [self.cardno, self.rfid])
-            r = self.cursor.fetchone()
-        except Exception, e:
-            raise Exception('Invalid card:%s' % [e])
-        return True if r == None else False
-
-class PhoenixLeagueCard(PhoenixCard):
-
-    def get_cardno(self):
-        if self.cardno:
-            return self.cardno
-        elif self.rfid:
-            cursor = connections['hi'].cursor()
-            try:
-                cursor.execute('SELECT cardno from lg_checkrfid where rfid=%s', [self.rfid])
-                r = cursor.fetchone()
-            except Exception:
-                raise Exception('invalid rfid number')
-            if r:
-                return str(r[0])
-            else:
-                raise Exception('invalid rfid number')
-        else:
-            raise Exception('cardno or rfid needs to be set')
-
-    def get_rfid(self):
-        if self.rfid:
-            return self.rfid
-        elif self.cardno:
-            cursor = connections['hi'].cursor()
-            try:
-                cursor.execute('SELECT rfid from lg_checkrfid where cardno=%s', [self.cardno])
-                r = cursor.fetchone()
-            except Exception:
-                raise Exception('invalid card number')
-            if r:
-                return str(r[0])
-            else:
-                raise Exception('invalid card number')
-        else:
-            raise Exception('cardno or rfid needs to be set')
-
-    def get_stat(self):
-        cursor = connections['hi'].cursor()
-        try:
-            cursor.execute('SELECT ppd_ta, mpr_ta from ml.luser a, lg_checkrfid b where a.rfid=getorigrfid2(b.rfid) and (b.cardno=%s or b.rfid=%s)', [self.cardno, self.rfid])
+            cursor.execute('SELECT cardno, rfid, getorigrfid2(%s) from checkrfid where cardno=%s or rfid=%s', [rfid, cardno, rfid])
             r = cursor.fetchone()
         except Exception, e:
-            raise Exception('invalid card:%s' % [e])
-        if r:
-            return {'PPD': r[0], 'MPR': r[1]}
+            raise Exception('db error: %s' % [e])
+        if not r:
+            raise Exception('Casual card variflication failed')
+        (cardno, rfid, org_rfid ) = r
+        return {'cardno': cardno, 'rfid': rfid, 'org_rfid': org_rfid}
+
+    def _card_info(self):
+        """Get card info.
+        There are 5 different card types:
+        1. blank/never used new one
+            no userinfo (name is null), no members(m_num is null)  and utime is null
+        2. used on console but not registered on the web
+            userinfo record but no members(m_num is null) and utime is still null
+        3. registered as a principal card
+
+        4. registered as an extra card
+
+        5. copied card
+
+        """
+        rfid = self.rfid
+        try:
+            cursor.execute("""SELECT %s, (select cardno from checkrfid where rfid=%s),
+                                a.cardno, a.rfid, b.rfid, c.rfid, name, m_num, utime,
+                                realname, m_sex, m_email, m_phone, m_id, m_zip, ppd_ta2, mpr_ta2
+                                FROM checkrfid a
+                                LEFT JOIN userinfo b on a.rfid=b.rfid
+                                LEFT JOIN members c on b.m_num=c.num
+                                LEFT JOIN useravg d on b.rfid=d.rfid
+                                WHERE a.rfid=getorigrfid2(%s)""", [rfid, rfid, rfid])
+
+            (rfid, cardno, old_cardno, old_rfid, userinfo_rfid, members_rfid, name,
+             m_num, utime, realname, m_sex, m_email, m_phone, m_id, m_zip, ppd, mpr) = cursor.fetchone()
+        except Exception, e:
+            raise Exception('card variflication failed: %s' % [e])
+
+        if m_sex == 1:
+            m_sex = 'F'
+        elif m_sex == 2:
+            m_sex = 'M'
         else:
-            raise Exception('invalid card')
+            m_sex = ''
+
+        if realname:
+            names = realname.split()
+            if len(names) == 1:
+                f_name = names[0]
+                l_name = ''
+            elif len(names) == 2:
+                f_name = names[0].strip()
+                l_name = names[1].strip()
+            elif len(names) >= 3:
+                f_name = names[0].strip()
+                l_name = names[2].strip()
+        else:
+            f_name = ''
+            l_name = ''
+
+        m_email = m_email.strip().lower() if m_email else ''
+
+        ppd = ppd if ppd else Decimal(0)
+        mpr = mpr if mpr else Decimal(0)
+
+        if not userinfo_rfid and not members_rfid and not utime:
+            # blank, never used one
+            card_type = 'new'
+        elif userinfo_rfid and name and not m_num and not utime:
+            # used on console but not registered on the web
+            card_type = 'temporary'
+        elif (userinfo_rfid == members_rfid) and m_num and m_id:
+            # principal card
+            card_type = 'principal'
+        elif (userinfo_rfid != members_rfid) and m_num and m_id:
+            card_type = 'extra'
+
+        return (card_type, rfid, cardno, old_cardno, old_rfid, userinfo_rfid, members_rfid, name,
+                m_num, utime, f_name, l_name, m_sex, m_email, m_phone, m_id, m_zip, ppd, mpr)
+
+
+    def get_stat(self):
+        cursor.execute('SELECT ppd_ta2, mpr_ta2 from useravg where rfid=getorigrfid2(%s))', [self.rfid])
+        (ppd, mpr) = cursor.fetchone()
+        return {'PPD': ppd, 'MPR': mpr}
+
+
+class PhoenixLeagueCard(PhoenixCard):
+    def __init__(self, cardno=None, rfid=None):
+        self._validate_number(cardno, rfid)
+        r = self._verify(cardno, rfid)
+
+        self.cardno = str(r['cardno'])
+        self.rfid = str(r['rfid'])
+
+        (name, nickname, ppd_ta, mpr_ta) = self._card_info()
+        self.ppd = ppd_ta
+        self.mpr = mpr_ta
+        self.nick_name = nickname
+        self.name = name
+
+    def _verify(self, cardno, rfid):
+            try:
+                cursor.execute('SELECT cardno, rfid from lg_checkrfid where cardno=%s or rfid=%s', [cardno, rfid])
+                r = cursor.fetchone()
+            except Exception, e:
+                raise Exception('card variflication failed: %s' % [e])
+            if not r:
+                raise Exception('League card variflication failed')
+            (cardno, rfid) = r
+            return {'cardno': cardno, 'rfid': rfid}
+
+    def _card_info(self):
+        cursor.execute('SELECT name, nickname, ppd_ta, mpr_ta from ml.luser a, ml.luserinfo b where a.rfid=b.rfid and a.rfid=%s', [self.rfid,])
+        (name, nickname, ppd_ta, mpr_ta) = cursor.fetchone()
+        return (name, nickname, ppd_ta, mpr_ta)
